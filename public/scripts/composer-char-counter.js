efa39a13e3b00a46e2c5176a450acecfa603dc61
Marina Ivanova <marina.ivanova.ca@gmail.com>
2023-07-10 13:35:13 -0700

    Refactor counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,27 +1,50 @@
-$(document).ready(function(){
+// $(document).ready(function(){
+//   console.log('composer-char-counter.js loaded successfully');
+// // Get the textarea element
+// const textarea = document.getElementById('tweet-text');
+// // Add event listener to the textarea
+// textarea.addEventListener('keyup', updateCharacterCount);
+// // Get the counter element
+// const counter = document.getElementsByClassName('counter');
+
+// // Function to update character count
+// function updateCharacterCount() {
+//   const text = $(this).val();
+//   const charCount = text.length;
+//   counter[0].textContent = charCount;
+//   const remainingCount = 140 - charCount;
+//   console.log(remainingCount);
+//   counter[0].textContent = remainingCount;
+//   if (remainingCount < 0) {
+//     counter[0].classList.add('exceeded');
+//   } else {
+//     counter[0].classList.remove('exceeded');
+//   }
+// }
+// });
+
+
+
+$(document).ready(function() {
   console.log('composer-char-counter.js loaded successfully');
-// Get the textarea element
-const textarea = document.getElementById('tweet-text');
-// Add event listener to the textarea
-textarea.addEventListener('input', updateCharacterCount);
-// Get the counter element
-const counter = document.getElementsByClassName('counter');
 
-// Function to update character count
-function updateCharacterCount() {
-  const text = $(this).val();
-  const charCount = text.length;
-  counter[0].textContent = charCount;
-  const remainingCount = 140 - charCount;
-  console.log(remainingCount);
-  counter[0].textContent = remainingCount;
-  if (remainingCount < 0) {
-    counter[0].classList.add('exceeded');
-  } else {
-    counter[0].classList.remove('exceeded');
+  // Get the textarea element
+  const textarea = $('#tweet-text');
+
+  // Add event listener to the textarea
+  textarea.on('keyup', updateCharacterCount);
+
+  // Function to update character count
+  function updateCharacterCount() {
+    const text = $(this).val();
+    const charCount = text.length;
+    const counter = $(this).next().find('.counter');
+    const remainingCount = 140 - charCount;
+    counter.text(remainingCount);
+    if (remainingCount < 0) {
+      counter.addClass('exceeded');
+    } else {
+      counter.removeClass('exceeded');
+    }
   }
-}
 });
-
-
-
